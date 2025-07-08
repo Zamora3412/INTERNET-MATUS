6607e8b8b951f8e381699cb5b287b8ef5a65179e
Zamora3412 <97114217+Zamora3412@users.noreply.github.com>
2025-07-08 11:25:20 -0600

    Delete INTERNET-MATUS directory

--- INTERNET-MATUS/Scrip/Login.js
+++
@@ -1,9 +0,0 @@
-function showLogin() {
-    document.getElementById("loginForm").style.display = "block";
-    document.getElementById("registerForm").style.display = "none";
-}
-
-function showRegister() {
-    document.getElementById("loginForm").style.display = "none";
-    document.getElementById("registerForm").style.display = "block";
-}// JavaScript Document
